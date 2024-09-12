2cbd197f085f152c4412443561047bc570dacd4c
Ruan <ruancalsolari93@gmail.com>
2024-09-12 12:19:45 -0300

    teste commit

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -8,7 +8,7 @@ import logo from '../../assets/sinalibras.png'
 
 const Login = () => {
 
-  const [username, setUsername] = useState("")
+  const [email, setUsername] = useState("")
   const [name, setName] = useState("")
   const [password, setPassword] = useState("")
   const [senha, setSenha2] = useState("")
@@ -17,57 +17,65 @@ const Login = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    alert ("Enviando os dados " + username + " - " + password + " - " + name + " - " + senha + " - " + date)
+    alert ("Enviando os dados " + email + " - " + name + " - " + password + " - " + senha + " - " + date)
 
   }
 
   return (
+
+    <><div className="left">
+      <img className='sina-libras-logo' src={logo} alt="Sina Libras Logo" />
+    </div>
+    
     <div className='container'>
 
+
+
       <form onSubmit={handleSubmit}>
-        
-        
+
+
         <img className='logo' src={imagembarros} alt="" />
 
         <div className='input-field'>
-        <input 
-        type="email" 
-        placeholder='E-mail' 
-        required 
-        onChange={(e) => setUsername(e.target.value)} />
-        <IoMdMail className='icon'/>
+          <input
+            type="email"
+            placeholder='E-mail'
+            required
+            id='email'
+            onChange={(e) => setUsername(e.target.value)} />
+          <IoMdMail className='icon' />
         </div>
 
         <div className='input-field'>
-        <input type="nome" placeholder='Nome' 
-        onChange={(e) => setName(e.target.value)} />
-        <FaUser className='icon'/>
-        </div>
-        
-        <div className='input-field'>
-        <input type="password" placeholder='Senha' 
-        onChange={(e) => setPassword(e.target.value)} />
-        <FaLock className='icon'/>
-        </div>
-        
-        <div className='input-field'>
-        <input type="password" placeholder='Confirmar Senha' 
-        onChange={(e) => setSenha2(e.target.value)}/>
-        <FaLock className='icon'/>
+          <input type="nome" id='nome' placeholder='Nome'
+            onChange={(e) => setName(e.target.value)} />
+          <FaUser className='icon' />
         </div>
 
         <div className='input-field'>
-        <input type="date" placeholder='00/00/0000'
-         onChange={(e) => setDate(e.target.value)} />
+          <input type="password" id='senha' placeholder='Senha'
+            onChange={(e) => setPassword(e.target.value)} />
+          <FaLock className='icon' />
+        </div>
+
+        <div className='input-field'>
+          <input type="password" is='confSenha' placeholder='Confirmar Senha'
+            onChange={(e) => setSenha2(e.target.value)} />
+          <FaLock className='icon' />
+        </div>
+
+        <div className='input-field'>
+          <input type="date" id='data' placeholder='00/00/0000'
+            onChange={(e) => setDate(e.target.value)} />
         </div>
 
 
-        
+
         <div>
           <button id='Cadastrar'>Cadastrar</button>
         </div>
       </form>
-    </div>
+    </div></>
   )
 }
 
